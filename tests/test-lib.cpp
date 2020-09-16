6ac9ce01ef89acebb6a881c5ef74458333cfd237
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-09-16 21:09:36 +0200

    Add test of shaders

--- tests/test-lib.cpp
+++ tests/test-lib.cpp
@@ -2,10 +2,30 @@
 #include <catch2/catch.hpp>
 #include <glove/lib.h>
 
-//TEST_CASE("Addition via add function", "[add]") {
-//	REQUIRE(add(0, 0) == 0);
-//
-//	SECTION("Positive numbers") { REQUIRE(add(1, 1) == 2); }
-//
-//	SECTION("Negative numbers") { REQUIRE(add(-1, -1) == -2); }
-//}
+/**
+ * Test if loading and compiling shaders doesn't throw exceptions or crash
+ */
+TEST_CASE("Compile Shader Programs", "[shaders]") {
+	auto window = Window("Test", 640, 480);
+
+	SECTION("Triangle shaders") {
+		auto shader =
+		    ShaderProgram({"shaders/triangle.vert", "shaders/triangle.frag"});
+		shader.use();
+	}
+
+	SECTION("Square shaders") {
+		auto shader =
+		    ShaderProgram({"shaders/square.vert", "shaders/square.frag"});
+		shader.use();
+		shader.setUniform("u_color", glm::vec3(1.0f, 1.0f, 1.0f));
+	}
+
+	SECTION("MVP shaders") {
+		auto shader = ShaderProgram({"shaders/mvp.vert", "shaders/mvp.frag"});
+		shader.use();
+		shader.setUniform("u_transform", glm::mat4(1.0f));
+		shader.setUniform("u_view", glm::mat4(1.0f));
+		shader.setUniform("u_projection", glm::mat4(1.0f));
+	}
+}
