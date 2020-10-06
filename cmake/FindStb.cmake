3e39c009a0a05754cdb58a12f9d5d1002fdb0175
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-10-06 10:39:26 +0200

    Add and use FindStb module

---
+++ cmake/FindStb.cmake
@@ -0,0 +1,58 @@
+# Distributed under the MIT License. See accompanying LICENSE.md
+
+#[=======================================================================[.rst:
+FindStb
+-------
+
+Finds the Stb headers.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``Stb::Stb``
+  Stb
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Stb_FOUND``
+  True if the system has the Stb.
+``Stb_INCLUDE_DIRS``
+  Include directory needed to use Stb.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Stb_INCLUDE_DIR``
+  The directory containing ``stb.h``.
+
+#]=======================================================================]
+include(FindPackageHandleStandardArgs)
+
+# Stb is a header only library, so all we need to do is find the directory in which it resides
+find_path(Stb_INCLUDE_DIR
+        NAMES "stb.h" "stb_image.h"
+        HINTS "/usr/include")
+
+find_package_handle_standard_args(Stb
+        FOUND_VAR Stb_FOUND
+        REQUIRED_VARS Stb_INCLUDE_DIR)
+
+if (Stb_FOUND)
+    message(STATUS "Stb found in: ${Stb_INCLUDE_DIR}")
+
+    # Classic Find*.cmake way
+    set(Stb_INCLUDE_DIRS ${Stb_INCLUDE_DIR})
+
+    # Newer *Config.cmake way
+    add_library(Stb::Stb INTERFACE IMPORTED)
+    target_include_directories(Stb::Stb INTERFACE ${Stb_INCLUDE_DIR})
+
+    mark_as_advanced(Stb_INCLUDE_DIR)
+endif ()
