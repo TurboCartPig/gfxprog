14ca67bbb56dd1110076f675dd7d92f651cf916b
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-10-07 21:05:18 +0200

    Fix tests and include pacman shader test

--- tests/test-lib.cpp
+++ tests/test-lib.cpp
@@ -10,22 +10,31 @@ TEST_CASE("Compile Shader Programs", "[shaders]") {
 
 	SECTION("Triangle shaders") {
 		auto shader =
-		    ShaderProgram({"shaders/triangle.vert", "shaders/triangle.frag"});
+		    ShaderProgram({"resources/shaders/triangle.vert", "resources/shaders/triangle.frag"});
 		shader.use();
 	}
 
 	SECTION("Square shaders") {
 		auto shader =
-		    ShaderProgram({"shaders/square.vert", "shaders/square.frag"});
+		    ShaderProgram({"resources/shaders/square.vert", "resources/shaders/square.frag"});
 		shader.use();
 		shader.setUniform("u_color", glm::vec3(1.0f, 1.0f, 1.0f));
 	}
 
 	SECTION("MVP shaders") {
-		auto shader = ShaderProgram({"shaders/mvp.vert", "shaders/mvp.frag"});
+		auto shader = ShaderProgram({"resources/shaders/mvp.vert", "resources/shaders/mvp.frag"});
 		shader.use();
 		shader.setUniform("u_transform", glm::mat4(1.0f));
 		shader.setUniform("u_view", glm::mat4(1.0f));
 		shader.setUniform("u_projection", glm::mat4(1.0f));
+	}
+
+	SECTION("Pacman shaders") {
+		auto shader = ShaderProgram(
+		    {"resources/shaders/pacman.vert", "resources/shaders/pacman.frag"});
+		shader.use();
+		shader.setUniform("u_view", glm::mat4(1.0f));
+		shader.setUniform("u_projection", glm::mat4(1.0f));
+		shader.setUniform("u_sprite_sheet", 0u);
 	}
 }
