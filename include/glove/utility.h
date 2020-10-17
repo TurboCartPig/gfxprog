076ba9e2b060a8c1b73b2333f1269227a3d5514c
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-10-17 18:00:35 +0200

    Add printing support to glm primitives

--- include/glove/utility.h
+++ include/glove/utility.h
@@ -1,8 +1,40 @@
 #pragma once
 
 #include <GL/glew.h>
+#include <glm/mat2x2.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 void GLAPIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id,
                               GLenum severity, [[maybe_unused]] GLsizei length,
                               const char *                 message,
                               [[maybe_unused]] const void *userParam);
+
+// Overload ostream to support printing glm primitives.
+
+std::ostream &operator<<(std::ostream &os, const glm::vec2 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::vec3 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::vec4 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec2 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec3 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec4 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec2 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec3 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec4 &vector);
+
+std::ostream &operator<<(std::ostream &os, const glm::mat2 &matrix);
+
+std::ostream &operator<<(std::ostream &os, const glm::mat3 &matrix);
+
+std::ostream &operator<<(std::ostream &os, const glm::mat4 &matrix);
--- lib/utility.cpp
+++ lib/utility.cpp
@@ -1,5 +1,9 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic   ignored "cppcoreguidelines-pro-type-union-access"
+
 #include <glove/utility.h>
 #include <iostream>
+#include <ostream>
 
 void GLAPIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id,
                               GLenum severity, [[maybe_unused]] GLsizei length,
@@ -57,3 +61,98 @@ void GLAPIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id,
 	}
 	std::cout << std::endl;
 }
+
+std::ostream &operator<<(std::ostream &os, const glm::vec2 &vector) {
+	os << "vec2(" << vector.x << ", " << vector.y << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::vec3 &vector) {
+	os << "vec3(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::vec4 &vector) {
+	os << "vec4(" << vector.x << ", " << vector.y << ", " << vector.z << ", "
+	   << vector.w << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec2 &vector) {
+	os << "ivec2(" << vector.x << ", " << vector.y << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec3 &vector) {
+	os << "ivec3(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::ivec4 &vector) {
+	os << "ivec4(" << vector.x << ", " << vector.y << ", " << vector.z << ", "
+	   << vector.w << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec2 &vector) {
+	os << "uvec2(" << vector.x << ", " << vector.y << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec3 &vector) {
+	os << "uvec3(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::uvec4 &vector) {
+	os << "uvec4(" << vector.x << ", " << vector.y << ", " << vector.z << ", "
+	   << vector.w << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::mat2 &matrix) {
+	os << "mat2("
+	   << "(" << matrix[0][0] << ", " << matrix[0][1] << "), "
+	   << "(" << matrix[1][0] << ", " << matrix[1][1] << ")"
+	   << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::mat3 &matrix) {
+	os << "mat3("
+	   << "(" << matrix[0][0] << ", " << matrix[0][1] << ", " << matrix[0][2]
+	   << "), "
+	   << "(" << matrix[1][0] << ", " << matrix[1][1] << ", " << matrix[1][2]
+	   << "), "
+	   << "(" << matrix[2][0] << ", " << matrix[2][1] << ", " << matrix[2][2]
+	   << ")"
+	   << ")";
+
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::mat4 &matrix) {
+	os << "mat4("
+	   << "(" << matrix[0][0] << ", " << matrix[0][1] << ", " << matrix[0][2]
+	   << ", " << matrix[0][3] << "), "
+	   << "(" << matrix[1][0] << ", " << matrix[1][1] << ", " << matrix[1][2]
+	   << ", " << matrix[1][3] << "), "
+	   << "(" << matrix[2][0] << ", " << matrix[2][1] << ", " << matrix[2][2]
+	   << ", " << matrix[2][3] << "), "
+	   << "(" << matrix[3][0] << ", " << matrix[3][1] << ", " << matrix[3][2]
+	   << ", " << matrix[3][3] << ")"
+	   << ")";
+
+	return os;
+}
+#pragma clang diagnostic pop
