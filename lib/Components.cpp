42093db792d0bde9e983d0ec6ef19d84601b37f0
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-11-06 19:33:37 +0100

    Fix camera to wall clipping

--- lib/Components.cpp
+++ lib/Components.cpp
@@ -7,7 +7,7 @@ CameraComponent::CameraComponent(float aspect, float vfov)
 glm::mat4
 CameraComponent::asViewProjection(const TransformComponent &transform) const {
 	auto projection =
-	    glm::perspective(glm::radians(vfov), aspect, 0.01f, 100.0f);
+	    glm::perspective(glm::radians(vfov), aspect, 0.0001f, 100.0f);
 
 	auto center = transform.translation +
 	              transform.rotation * glm::vec3(0.0f, 0.0f, 1.0f);
