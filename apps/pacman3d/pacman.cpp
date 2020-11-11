ffa2c3d99d32b7691f398917f325460f2c0eafce
Dennis Kristiansen <denniskristiansen@protonmail.com>
2020-11-11 02:15:30 +0100

    Add wip minimap

--- apps/pacman3d/pacman.cpp
+++ apps/pacman3d/pacman.cpp
@@ -1,9 +1,7 @@
 #include "Entities.h"
 #include "Level.h"
 
-#include <fstream>
 #include <glove/lib.h>
-#include <unordered_map>
 
 /**
  * @brief Main game state of pacman 3d.
@@ -15,6 +13,16 @@ class GameState : public IGameState {
 
 		// Setup opengl state
 		// FIXME: Do this more intelligently
+
+		m_backbuffer = Framebuffer::defaultFramebuffer();
+
+		m_framebuffer = std::make_unique<Framebuffer>();
+		m_framebuffer->bind();
+		m_framebuffer->addAttachment(AttachmentType::Color,
+		                             glm::ivec2(240, 240));
+		m_framebuffer->addAttachment(AttachmentType::DepthStencil,
+		                             glm::ivec2(240, 240));
+		assert(m_framebuffer->valid());
 
 		// Enable depth testing
 		glEnable(GL_DEPTH_TEST);
@@ -71,9 +79,13 @@ class GameState : public IGameState {
 	}
 
 	void render() override {
+		// First render pass - Draw scene to the backbuffer
+		// **********************************************************************************************************
+		m_backbuffer->bind();
+
 		auto view       = m_pacman->view();
 		auto projection = m_pacman->projection();
-
+//
 		m_shader_program->use();
 		m_shader_program->setUniform("u_view", view);
 		m_shader_program->setUniform("u_projection", projection);
@@ -84,6 +96,38 @@ class GameState : public IGameState {
 		// for (auto &ghost : m_ghosts) {
 		// 	ghost.draw();
 		// }
+
+		// Second render pass - Draw scene to minimap and blit it to the backbuffer
+        // **********************************************************************************************************
+		m_framebuffer->bind();
+		m_framebuffer->clear();
+
+		glViewport(0, 0, 240, 240);
+
+		// Draw stuff
+		auto [w, h] = m_level->getSize();
+		view        = glm::lookAt(glm::vec3(0.0f, 10.0f, 0.0f),
+                           glm::vec3(0.0f, 0.0f, 0.0f),
+                           glm::vec3(0.0f, 0.0f, 1.0f));
+		projection  = glm::ortho(-(float)w, 0.0f, 0.0f, (float)h, 0.0f, 10.0f);
+
+		m_shader_program->use();
+		m_shader_program->setUniform("u_view", view);
+		m_shader_program->setUniform("u_projection", projection);
+
+		m_maze->draw();
+		m_pellets->draw(view, projection);
+
+		m_backbuffer->blit(m_framebuffer.get(), glm::ivec4(0, 0, 240, 240),
+		                   glm::ivec4(0, 0, 240, 240));
+
+		glViewport(0, 0, 1280, 720);
+
+		// Render pass end
+        // **********************************************************************************************************
+
+		// Bind the backbuffer for GLFW to read from
+		m_backbuffer->bind();
 	}
 
   private:
@@ -94,7 +138,10 @@ class GameState : public IGameState {
 	std::vector<Ghost>       m_ghosts;  ///< All the ghosts in the level.
 
 	std::unique_ptr<ShaderProgram>
-	    m_shader_program; ///< Default shader program (Used for the maze)
+	    m_shader_program; ///< Default shader program (Used for the maze).
+	std::unique_ptr<Framebuffer> m_framebuffer;
+	std::unique_ptr<Framebuffer>
+	    m_backbuffer; ///< Default framebuffer created by GLFW.
 };
 
 auto main() -> int {
